7d03b52723b74b6b5cbfdeb058bcf79e1cf1caaa
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-01-22 01:44:00 +0530

    chore: update middleware with try-catch

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -4,13 +4,12 @@ import { JWT_SECRET } from "@repo/backend-common/config";
 
 export const middleware = (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers["authorization"]?? "";
-    const decoded = jwt.verify(token, JWT_SECRET);
 
     try{
-        if (decoded) {
-           //@ts-ignore
-           req.userId = decoded.userId;
-        }
+        const decoded = jwt.verify(token, JWT_SECRET);
+        //@ts-ignore
+        req.userId = decoded.userId;
+        next();
     }catch (e){
         res.status(403).json({
             message: "unauthorized"
