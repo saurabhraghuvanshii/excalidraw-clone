ef2c0dae8c276bf3b9d3d74e7224421b90c51bee
saurabhraghuvanshii <saurabhsraghuvanshi@gmail.com>
2025-04-16 22:43:06 +0530

    sigup buttom  made simple

--- apps/excelidraw-fronted/app/page.tsx
+++ apps/excelidraw-fronted/app/page.tsx
@@ -31,7 +31,7 @@ const Index = () => {
               <Link href={"/signup"}>
                 <Button
                   variant="outline"
-                  className="h-12 px-6 bg-transparent border-[#9b87f5] text-[#D6BCFA] hover:bg-[#9b87f5]/10 hover:text-white transition-all"
+                  className="h-12 px-6 bg-gradient-to-r from-[#8B5CF6] to-[#7E69AB] hover:from-[#9b87f5] hover:to-[#8B5CF6] border-none shadow-lg shadow-purple-900/20 text-white"
                 >
                   Sign up
                 </Button>
